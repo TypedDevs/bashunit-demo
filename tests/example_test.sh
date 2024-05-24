b7765c36b14d3f59d527fb5344a6a2b151c7533c
Chemaclass <chemaclass@outlook.es>
2024-05-25 00:11:16 +0200

    red: mkhd does not create any directory yet

--- tests/example_test.sh
+++ tests/example_test.sh
@@ -2,4 +2,8 @@
 
 function test_mkhd_creates_hexagonal_directories() {
   ./src/mkhd
+
+  assert_directory_exists application
+  assert_directory_exists domain
+  assert_directory_exists infrastructure
 }
