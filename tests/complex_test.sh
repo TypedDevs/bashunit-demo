3956aa0d674f388ad5d245c30cc67ef7127570e0
Chemaclass <chemaclass@outlook.es>
2024-05-25 20:21:30 +0200

    test: complex_output_with_one_arg

--- tests/complex_test.sh
+++ tests/complex_test.sh
@@ -5,3 +5,9 @@ function test_complex_output_without_args() {
 
     assert_match_snapshot "$(./src/complex)"
 }
+
+function test_complex_output_with_one_arg() {
+    mock curl cat ./tests/fixtures/pull_request_api
+
+    assert_match_snapshot "$(./src/complex --one-argument)"
+}
